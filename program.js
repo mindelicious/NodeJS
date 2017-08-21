8eb725ee1be0899e6e0cdd2508eb3ec74127b688
mindelicious <danieluk29@gmail.com>
2017-08-21 14:52:34 +0200

    improvment

--- program.js
+++ program.js
@@ -13,12 +13,13 @@ process.stdin.on('readable', function() {
                 console.log('Name: ' + process.env.LOGNAME);
                 console.log('Node v: ' + process.version);
                 if (process.platform == 'darwin') {
-                    console.log('This is OS platform');
+                    console.log('This is MacOS platform');
                 } else if (process.platform == 'linux') {
                     console.log('This is Linux platform');
                 } else if (process.platform == 'win32' || process.platform == 'win64') {
                     console.log('This is Windows bro');
-                }
+                } else {
+                    console.log('What kind of OS you have?');
                 break;
             default:
             process.stderr.write('Wrong instruction bro!');
