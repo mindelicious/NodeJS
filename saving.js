63ad6f985f65d07a96f7e99c06a8c71669c38464
Matt <[mailprogramisty@gmail.com]>
2017-08-27 19:39:15 +0200

    v2.0

--- saving.js
+++ saving.js
@@ -1,12 +1,7 @@
 var fs = require('fs');
 
-fs.readdir('/.text/', function() {
-    fs.readFile('./text/readText.txt', 'utf-8', function(err, data) {
-        if(err) {
-            console.log(err);
-        }else{
-        console.log(data);
-    }
-    fs.writeFileSync('./text/writeText.txt', data);
-    });
+var fs = require('fs');
+
+fs.readdir('./text', function(err, data) {
+    fs.writeFileSync('./text/writeText.txt', data.join(', '));
 }); 
